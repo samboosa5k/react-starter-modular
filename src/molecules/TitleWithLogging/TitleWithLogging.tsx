5623e34c1655f2e77e60aa78204ecdadbbb359e7
Jasper V <jasper.verbon@gmail.com>
2023-12-02 19:37:14 +0100

    [refactor] trunk - Cleanup of comments and documentation

--- src/molecules/TitleWithLogging/TitleWithLogging.tsx
+++ src/molecules/TitleWithLogging/TitleWithLogging.tsx
@@ -2,9 +2,13 @@ import { withPropsLogging } from '@HOC/withPropsLogging';
 import { Title } from '@atoms/Title';
 
 export const TitleWithLogging = withPropsLogging(({ title, logProps }) => {
-        return (<>
-            <h2 onClick={(e) => logProps(e.currentTarget.innerText)}>Title With Logging</h2>
-            {Title({ title, logProps })}
-        </>);
+        return (
+            <>
+                <h2 onClick={(e) => logProps(e.currentTarget.innerText)}>
+                    Title With Logging
+                </h2>
+                <Title title={title} />
+            </>
+        );
     },
 );
